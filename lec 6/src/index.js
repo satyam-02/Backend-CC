79859487111c933d49666a521c59070de1201449
Satyam Gupta <satyam.gupta292002@gmail.com>
2024-01-16 18:58:53 +0530

    Add files via upload

---
+++ lec 6/src/index.js
@@ -0,0 +1,42 @@
+// try,catch and async await in connecting with db always
+
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+connectDB()
+
+
+
+
+// import { express } from "express";
+// const app=express()
+
+// (async()=>{
+//     try{
+//       await mongoose.connect(`${process.env.MONGODB_URI}/
+//       ${DB_NAME}`)
+//       app.on("error",(error)=>{
+//         console.log("ERROR",error);
+//         throw error
+//       })
+     
+//       app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on post $
+//         {process.env.PORT}`);
+//       })
+
+//     } catch (error){
+//        console.error("ERROR:",error)
+//        throw err
+//     }
+// })
