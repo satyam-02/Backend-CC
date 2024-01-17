744b48580f59ca550f308d27885a6229334a467e
Satyam Gupta <satyam.gupta292002@gmail.com>
2024-01-18 00:16:55 +0530

    Add files via upload

---
+++ lec 9/src/app.js
@@ -0,0 +1,24 @@
+import { Express } from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+
+app.use(express.json({
+    limit:"16kb" 
+}))
+
+app.use(express.urlencode({extended:true,limit:"16kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+ 
+
+export {app}
